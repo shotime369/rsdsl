b4bf876c0693a116864063a9fd08846e43bfcce4
shotime369 <143595834+shotime369@users.noreply.github.com>
2024-11-06 13:11:48 +0000

    update1

--- src/indexscript.js
+++ src/indexscript.js
@@ -1,4 +1,4 @@
 document.getElementById('login-form').onsubmit = function(event) {
-    event.preventDefault(); // Prevents the default form submission
+    event.preventDefault(); // Prevents the default form submission - currently switched off
     window.location.href = 'home.html';
 };
